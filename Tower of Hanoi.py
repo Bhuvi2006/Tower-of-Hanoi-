3e3f228ac21d52f854f0854af631f250ca065e12
Bhuvi21 <bhuvijai2006@gmail.com>
2024-06-20 09:17:29 +0530

    Add files via upload

---
+++ Tower of Hanoi.py
@@ -0,0 +1,11 @@
+#Tower of Hanoi(Has 3 rods"A","B","C")
+def tower(n,a,b,c):
+#Here n is the number of discs , a is the starting ,b is intermidiate and c is the final rod 
+    if n==1:
+        print("Move the first disc from",a,"to",c)
+        return
+    tower(n-1,a,c,b)
+    print("Move",n,"from",a,"to",c)
+    tower(n-1,b,a,c)
+n=int(input("Enter number of discs: "))
+tower(n,"A","B","C")
